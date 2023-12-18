4c520c8ee3b4d472d6e48cb9cfb52df6b6576931
Roger <r.holden@chi.ac.uk>
2023-12-18 11:40:39 +0000

    band 1 and 2 hard coded.

--- assignment/list.php
+++ assignment/list.php
@@ -1,17 +1,10 @@
 
-<h1>Convert JSON to array and loop through</h1>
+<h1>Employees</h1>
 <?php
 opcache_reset();  // stop caching
-
-function calc_band($salary) {
-
-
-    if ($salary < 10000) $band =1;
-    elseif ($salary > 10000 and $salary < 40000) $band = 2;
-
-
-    return $band;
-}
+error_reporting(E_ALL);
+ini_set('display_errors', 1);    // this 1 / 0
+ini_set('display_startup_errors', 1);
 
 // read emp JSON into array
 // read tax tables JSON into array
@@ -20,8 +13,70 @@ function calc_band($salary) {
 // get salary
 // work out the band
 
+function calc_band($salary){
+
+    if ($salary < 10000 ) $band = 1;
+
+    else {
+
+        if ($salary > 10000 AND $salary < 40000) $band = 2;
+        elseif  ($salary > 40000 AND $salary < 150000)  $band = 3;
+        else  $band = 4;
+    }
+    
+
+    return $band;
+
+}  // end calc_band()
+
+
+function calc_tax($salary, $band) {
+
+    $netsalary = 0;
+    // error checking!
+
+    // first tax band - no tax to pay (or net salary = gross)
+    if ($band == 1 ) {
+
+        $netsalary = $salary;
+        return $netsalary;
+
+    }
+
+
+    if ($band == 2 ) {
+
+        // deduct 10000 from salary
+        // the remainder is taxed at 20%
+
+        $taxable_salary = $salary - 10000;
+
+        $tax = $taxable_salary * 0.2;
+
+        echo "taxable = " . $taxable_salary . " ";
+        echo "tax = " . $tax . " ";
+
+
+        $netsalary = $salary - $tax;
+        return $netsalary;
+
+    }
+
+    
+
+    else {
+
+        $netsalary = 55555555555;
+        return $netsalary;
+    }
+
+
+} // end calc_tax()
+
 $jsonfile = 'data/employees-final.json';  // location of file
+
 $json = file_get_contents($jsonfile);     // read contents into a variable
+
 $emparray = json_decode($json, true);      // convert to an array
 
 // echo '<pre>'; print_r($emparray); echo '</pre>';
@@ -29,17 +84,30 @@ $emparray = json_decode($json, true);      // convert to an array
 
 foreach($emparray as $value ) {
 
-    echo $value['firstname'] . ' : ' . $value['salary'] . '<br>';
+    echo $value['lastname'] .  ' ';
 
     $salary = $value['salary'];
 
-    $band = calc_band($salary);
-
-    $netsalary = calc_tax($band, $salary);
-
-    echo $netsalary;    
 
     // 1. work out tax band
+
+    $band = 0;
+
+    
+    echo $salary; 
+   
+    $band = calc_band($salary);
+
+    echo " The band is " . $band;
+
+    $netsalary = calc_tax($salary, $band);
+    
+    echo " ::  the gross (take-home) pay is  " . $netsalary / 12;
+
+
+    echo '<br><hr>';
+
+
     // 2. work out the tax
  
      
